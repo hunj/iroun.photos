d9073fa7fcca4f32a65d27e6983606371cb264f3
hunj <hunjaelee@protonmail.com>
2023-08-18 13:53:21 -0400

    fix typo

--- web/common/models.py
+++ web/common/models.py
@@ -3,6 +3,9 @@ from uuid import uuid4
 
 
 class BaseModel(models.Model):
-    uuid = models.UUIDField(priamry_key=True, default=uuid4, editable=False)
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
