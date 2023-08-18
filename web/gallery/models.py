bf6e356419b9a6a016e50d844af5f3b6e9e8c027
hunj <hunjaelee@protonmail.com>
2023-08-18 13:53:33 -0400

    fix model definition errors

--- web/gallery/models.py
+++ web/gallery/models.py
@@ -3,9 +3,9 @@ from common.models import BaseModel
 
 
 class Gallery(BaseModel):
-    name = models.CharField()
+    name = models.CharField(max_length=256)
 
 
 class Photo(BaseModel):
-    gallery = models.ForeignKey(Gallery)
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE)
     file = models.ImageField(upload_to='photos/')
