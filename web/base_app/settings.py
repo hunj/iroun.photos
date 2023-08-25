fe31fd35cf31e4065bcbc98059d93d6c8763a8bf
hunj <hunjaelee@protonmail.com>
2023-08-25 16:43:19 -0400

    `pages` app to handle static pages

--- web/base_app/settings.py
+++ web/base_app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
+    'pages',
     'gallery',
 ]
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
