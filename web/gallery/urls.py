c63ef3fad4501c68e8541d996358d68139f2c0f1
hunj <hunjaelee@protonmail.com>
2023-08-25 16:44:21 -0400

    fix url name conflict

--- web/gallery/urls.py
+++ web/gallery/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.AlbumListView.as_view(), name="gallery_list"),
     path("<uuid:uuid>", views.AlbumDetailView.as_view(), name="gallery_details"),
-    path("<uuid:gallery_uuid>/photo/<uuid:photo_uuid>", views.PhotoDetailView.as_view(), name="gallery_details"),
+    path("<uuid:gallery_uuid>/photo/<uuid:photo_uuid>", views.PhotoDetailView.as_view(), name="gallery_photo_details"),
 ]
