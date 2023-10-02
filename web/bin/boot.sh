f285bd2a20ece7a48d007842c56b5fdd7c78aa96
hunj <hunjaelee@protonmail.com>
2023-10-02 10:27:18 -0400

    :boot: use shell file to kick off gunicorn

--- web/bin/boot.sh
+++ web/bin/boot.sh
@@ -14,4 +14,15 @@ fi
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-exec "$@"
+start_gunicorn() {
+    # Generate the reload-extra-file options dynamically
+    extra_files=$(find /web -name "*.html" -printf "--reload-extra-file %p ")
+
+    # Start Gunicorn
+    echo "Starting Gunicorn..."
+    gunicorn base_app.wsgi:application --bind 0.0.0.0:8001 --reload --reload-engine=poll $extra_files
+}
+
+# Start Gunicorn
+start_gunicorn
+
