0a998235930a9d5de3ffee3ce66863432260da79
hunj <hunjaelee@protonmail.com>
2023-10-10 10:40:19 -0400

    handle uploaded files

--- web/base_app/settings.py
+++ web/base_app/settings.py
@@ -131,6 +131,13 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 
+
+# Uploaded files
+
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = '/uploads/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
