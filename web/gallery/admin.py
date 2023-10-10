64d794b39343869e7c8c4f29b6f3434d9726418a
hunj <hunjaelee@protonmail.com>
2023-10-10 10:41:21 -0400

    create model admin to easily work with on /admin

--- web/gallery/admin.py
+++ web/gallery/admin.py
@@ -1,3 +1,33 @@
+from django import forms
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+
+from gallery.models import Photo, Album
+
+
+class AlbumAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cover'].queryset = Photo.objects.filter(album=self.instance)
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    form = AlbumAdminForm
+    fields = ["name", "published", "cover"]
+
+    def preview(self, obj):
+        if obj.cover:
+            return format_html(f"<img src='{obj.cover.file.url}' width=200>")
+
+    list_display = ['name', 'preview',]
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    def preview(self, obj):
+        return format_html(f"<img src='{obj.file.url}' width=200>")
+
+    fields = ["album", "file", "visible"]
+    list_display = ['uuid', 'preview',]
