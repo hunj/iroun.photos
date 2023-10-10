b4fc9cf36c82eb6fef395db916fba7b1142dfd92
hunj <hunjaelee@protonmail.com>
2023-10-10 10:41:51 -0400

    simple views

--- web/gallery/views.py
+++ web/gallery/views.py
@@ -1,16 +1,23 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render
-
 from gallery.models import Album, Photo
 
 
 class AlbumListView(ListView):
     model = Album
+    queryset = Album.objects.filter(published=True)
 
 
 class AlbumDetailView(DetailView):
     model = Album
+    pk_url_kwarg = "gallery_uuid"
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        current_album = self.get_object()
+        context_data['photos'] = Photo.objects.filter(album=current_album, visible=True)
+        return context_data
 
 
 class PhotoDetailView(DetailView):
     model = Photo
+    pk_url_kwarg = "photo_uuid"
