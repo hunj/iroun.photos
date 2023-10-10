f33008cf4e10813c1b97821fabaecd4c30d664f5
hunj <hunjaelee@protonmail.com>
2023-10-10 10:40:52 -0400

    consistent naming

--- web/gallery/urls.py
+++ web/gallery/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.AlbumListView.as_view(), name="gallery_list"),
-    path("<uuid:uuid>", views.AlbumDetailView.as_view(), name="gallery_details"),
+    path("<uuid:gallery_uuid>", views.AlbumDetailView.as_view(), name="gallery_details"),
     path("<uuid:gallery_uuid>/photo/<uuid:photo_uuid>", views.PhotoDetailView.as_view(), name="gallery_photo_details"),
 ]
