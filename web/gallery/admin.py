ef80b6527340c0230a50734b3a50c878b7dcba9b
hunj <hunjaelee@protonmail.com>
2023-10-15 15:36:23 -0400

    add admin form

--- web/gallery/admin.py
+++ web/gallery/admin.py
@@ -2,8 +2,13 @@ from django import forms
 from django.contrib import admin
 from django.utils.html import format_html
 
-
 from gallery.models import Photo, Album
+
+
+class PhotoAdminForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = "__all__"
 
 
 class AlbumAdminForm(forms.ModelForm):
@@ -19,15 +24,16 @@ class AlbumAdmin(admin.ModelAdmin):
 
     def preview(self, obj):
         if obj.cover:
-            return format_html(f"<img src='{obj.cover.file.url}' width=200>")
+            return format_html(f"<img src='{obj.cover.file.url}' width='200'>")
 
     list_display = ['name', 'preview',]
+
 
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
     def preview(self, obj):
-        return format_html(f"<img src='{obj.file.url}' width=200>")
+        return format_html(f"<img src='{obj.file.url}' width='200'>")
 
     fields = ["album", "file", "visible"]
     list_display = ['uuid', 'preview',]
