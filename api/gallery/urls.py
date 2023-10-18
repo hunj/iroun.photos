54e6b7c872d5e608b8927a4e54f25d08daa9a303
Hun Jae Lee <hunjaelee@protonmail.com>
2023-10-18 18:12:27 -0400

    gallery -> album

--- api/gallery/urls.py
+++ api/gallery/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.AlbumListAPIView.as_view(), name="gallery_list"),
-    path("<uuid:gallery_uuid>", views.AlbumRetrieveAPIView.as_view(), name="gallery_details"),
-    path("<uuid:gallery_uuid>/photo/<uuid:photo_uuid>", views.PhotoRetrieveAPIView.as_view(), name="gallery_photo_details"),
+    path("", views.AlbumListAPIView.as_view(), name="album_list"),
+    path("<uuid:album_uuid>", views.AlbumRetrieveAPIView.as_view(), name="album_details"),
+    path("<uuid:album_uuid>/photo/<uuid:photo_uuid>", views.PhotoRetrieveAPIView.as_view(), name="album_photo_details"),
 ]
