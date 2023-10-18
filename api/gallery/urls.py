d7f22f16530f6e22b0208ddb8bfee888746890ca
Hun Jae Lee <hunjaelee@protonmail.com>
2023-10-18 16:01:58 -0400

    begin converting views to apiviews

--- api/gallery/urls.py
+++ api/gallery/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.AlbumListView.as_view(), name="gallery_list"),
-    path("<uuid:gallery_uuid>", views.AlbumDetailView.as_view(), name="gallery_details"),
-    path("<uuid:gallery_uuid>/photo/<uuid:photo_uuid>", views.PhotoDetailView.as_view(), name="gallery_photo_details"),
+    path("", views.AlbumListAPIView.as_view(), name="gallery_list"),
+    path("<uuid:gallery_uuid>", views.AlbumRetrieveAPIView.as_view(), name="gallery_details"),
+    path("<uuid:gallery_uuid>/photo/<uuid:photo_uuid>", views.PhotoRetrieveAPIView.as_view(), name="gallery_photo_details"),
 ]
--- api/gallery/views.py
+++ api/gallery/views.py
@@ -1,10 +1,38 @@
 from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+from gallery.serializers import AlbumListSerializer, AlbumRetrieveSerializer, PhotoSerializer
 from gallery.models import Album, Photo
 
 
-class AlbumListView(ListView):
+class AlbumListAPIView(ListAPIView):
     model = Album
     queryset = Album.objects.filter(published=True)
+    serializer_class = AlbumListSerializer
+
+
+class AlbumRetrieveAPIView(RetrieveAPIView):
+    model = Album
+    lookup_field = 'uuid'
+    lookup_url_kwarg = 'gallery_uuid'
+    serializer_class = AlbumRetrieveSerializer
+
+    def get_queryset(self):
+        queryset = Album.objects.filter(published=True).prefetch_related('photos')
+        return queryset
+
+
+class PhotoRetrieveAPIView(RetrieveAPIView):
+    model = Photo
+    lookup_field = 'uuid'
+    lookup_url_kwarg = 'photo_uuid'
+    serializer_class = PhotoSerializer
+
+    def get_queryset(self):
+        gallery_uuid = self.kwargs.get('gallery_uuid')
+        queryset = Photo.objects.filter(album__uuid=gallery_uuid)
+        return queryset
 
 
 class AlbumDetailView(DetailView):
