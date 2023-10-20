fb094878311b300731a3aafa3c852d0331808a81
hunj <hunjaelee@protonmail.com>
2023-10-20 15:51:07 -0400

    create hacky admin view for album to mass-upload photos

--- web/gallery/admin.py
+++ web/gallery/admin.py
@@ -1,14 +1,22 @@
 from django import forms
 from django.contrib import admin
 from django.utils.html import format_html
+from django.urls import path, reverse
 
 from gallery.models import Photo, Album
+from gallery.views import PhotoMultiUploadAdminView
 
 
-class PhotoAdminForm(forms.ModelForm):
-    class Meta:
-        model = Photo
-        fields = "__all__"
+class PhotoInline(admin.TabularInline):
+    model = Photo
+    fields = ("file", "preview", "visible",)
+    readonly_fields = ("preview",)
+    extra = 0
+
+    @admin.display()
+    def preview(self, obj):
+        if obj.file:
+            return format_html(f"<img src='{obj.file.url}' height='200'>")
 
 
 class AlbumAdminForm(forms.ModelForm):
@@ -17,23 +25,37 @@ class AlbumAdminForm(forms.ModelForm):
         self.fields['cover'].queryset = Photo.objects.filter(album=self.instance)
 
 
-@admin.register(Album)
-class AlbumAdmin(admin.ModelAdmin):
-    form = AlbumAdminForm
-    fields = ["name", "published", "cover"]
-
-    def preview(self, obj):
-        if obj.cover:
-            return format_html(f"<img src='{obj.cover.file.url}' width='200'>")
-
-    list_display = ['name', 'preview',]
-
-
 @admin.register(Photo)
 class PhotoAdmin(admin.ModelAdmin):
-
     def preview(self, obj):
-        return format_html(f"<img src='{obj.file.url}' width='200'>")
+        return format_html(f"<img src='{obj.file.url}' height='200'>")
 
     fields = ["album", "file", "visible"]
     list_display = ['uuid', 'preview',]
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    form = AlbumAdminForm
+    list_display = ['name', 'preview', 'upload',]
+    inlines = [PhotoInline]
+
+    @admin.display()
+    def preview(self, obj):
+        if obj.cover:
+            return format_html(f"<img src='{obj.cover.file.url}' height='200'>")
+
+    def get_urls(self):
+        urls = super().get_urls()
+        urls.append(
+            path(
+                "<pk>/upload",
+                self.admin_site.admin_view(PhotoMultiUploadAdminView.as_view()),
+                name="album_photo_multiupload",
+            )
+        )
+        return urls
+
+    def upload(self, obj):
+        url = reverse("admin:album_photo_multiupload", args=[obj.pk])
+        return format_html(f'<a href="{url}">Upload Photos</a>')
--- web/gallery/views.py
+++ web/gallery/views.py
@@ -1,3 +1,8 @@
+from django.contrib import admin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from django.views.generic import ListView, DetailView
 from gallery.models import Album, Photo
 
@@ -21,3 +26,24 @@ class AlbumDetailView(DetailView):
 class PhotoDetailView(DetailView):
     model = Photo
     pk_url_kwarg = "photo_uuid"
+
+
+class PhotoMultiUploadAdminView(PermissionRequiredMixin, DetailView):
+    permission_required = 'gallery.edit_album'
+    template_name = 'admin/photo_upload_admin.html'
+    model = Album
+
+    def get_context_data(self, **kwargs):
+        return {
+            **super().get_context_data(**kwargs),
+            **admin.site.each_context(self.request),
+            "opts": self.model._meta,
+        }
+
+    def post(self, request, *args, **kwargs):
+        uploaded_files = request.FILES.getlist('photos')
+        current_album = self.get_object()
+        for file in uploaded_files:
+            p = Photo(file=file, album=current_album)
+            p.save()
+        return HttpResponseRedirect(reverse("admin:gallery_album_change", args=[current_album.pk]))
