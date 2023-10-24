f10d223ed91ae2ec30e5b57c8e9029d39293993e
Hun Jae Lee <hunjaelee@protonmail.com>
2023-10-24 11:35:44 -0400

    WIP url paths for new models

--- web/gallery/urls.py
+++ web/gallery/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path("", views.AlbumListView.as_view(), name="gallery_list"),
     path("<uuid:gallery_uuid>", views.AlbumDetailView.as_view(), name="gallery_details"),
     path("<uuid:gallery_uuid>/photo/<uuid:photo_uuid>", views.PhotoDetailView.as_view(), name="gallery_photo_details"),
+
+    # path("tagged/<slug:person_slug>", views, name="search_by_tag"),
+    # path("event/<slug:event_slug>", views, name="search_by_event"),
+    # path("location/<slug:location_slug>", views, name="search_by_location"),
 ]
