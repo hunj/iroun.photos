7148b06764aa8a1f5a70b6fae5760e492dbe15dc
Hun Jae Lee <hunjaelee@protonmail.com>
2023-10-30 13:51:15 -0400

    add ListView for tagged person

--- web/gallery/urls.py
+++ web/gallery/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("<slug:gallery_slug>", views.AlbumDetailView.as_view(), name="gallery_details"),
     path("<slug:gallery_slug>/<uuid:photo_uuid>", views.PhotoDetailView.as_view(), name="gallery_photo_details"),
 
-    # path("tagged/<slug:person_slug>", views, name="search_by_tag"),
+    path("tagged/<slug:person_slug>", views.TaggedListView.as_view(), name="search_by_tag"),
     # path("event/<slug:event_slug>", views, name="search_by_event"),
     # path("location/<slug:location_slug>", views, name="search_by_location"),
 ]
