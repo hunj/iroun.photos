8387219c3ed04424154f9d90a07e4fb644b1f697
Hun Jae Lee <hunjaelee@protonmail.com>
2023-11-07 19:12:08 -0500

    cleanup

--- api/pages/urls.py
+++ api/pages/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
-
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('about', TemplateView.as_view(template_name='about.html'), name='about'),
