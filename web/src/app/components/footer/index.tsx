22ed9d043f6fa804000f8d9dd0f90ece6b5d998b
Hun Jae Lee <hunjaelee@protonmail.com>
2023-11-24 15:40:44 -0500

    remove sticky footer

--- web/src/app/components/footer/index.tsx
+++ web/src/app/components/footer/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="sticky bottom-0 py-6 bg-gray-100">
+        <footer className="bottom-0 py-6 bg-gray-100 mt-6">
             <div className="text-center">
                 <p>
                     <i className="fa fa-copyright" aria-hidden="true"></i> 2023 <a href="/" className="font-bold text-blue-400">iroun.photos</a> / <a href="https://hunj.tech/" className="text-blue-400">Hun Jae Lee</a>. All rights reserved.
