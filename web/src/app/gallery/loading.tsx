01172a1f7fdfc4b7d698813ccd54139c61329236
Hun Jae Lee <hunjaelee@protonmail.com>
2023-11-24 13:23:51 -0500

    fix fontawesome spinner

--- web/src/app/gallery/loading.tsx
+++ web/src/app/gallery/loading.tsx
@@ -1,11 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 export default function Loading() {
     return (
-    <section className="h-full">
-        <div className="mx-auto">
-            <FontAwesomeIcon icon="spinner" />
-        </div>
-    </section>
+        <section className="h-full">
+            <div className="mx-auto">
+                <FontAwesomeIcon icon={faSpinner} />
+            </div>
+        </section>
     );
 }
