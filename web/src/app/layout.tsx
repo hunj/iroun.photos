cfb3714e1abb7736ce1fc9db0b9a552d2bf1b39a
Hun Jae Lee <hunjaelee@protonmail.com>
2023-11-29 11:18:07 -0500

    add goog anal

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import Navigation from "./components/navigation"
 import Footer from "./components/footer"
 import type { Metadata } from 'next'
@@ -13,9 +14,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID
+
   return (
     <html lang="en">
       <body className="flex flex-col h-screen">
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+        </Script>
         <Navigation />
         {children}
         <Footer />
